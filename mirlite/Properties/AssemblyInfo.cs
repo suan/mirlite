bb7dee28b4d8e4eb875fa506784f4c929d4c26fc
Suan-Aik Yeo <yeosuanaik@gmail.com>
2010-10-30 11:13:34 -0500

    Changed description that appears in Task Manager

--- mirlite/Properties/AssemblyInfo.cs
+++ mirlite/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("testbed")]
+[assembly: AssemblyTitle("Violet mir:ror configuration software")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("testbed")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyProduct("mir:lite")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
